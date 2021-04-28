e21bb3f780b5fdad5890273553667fff5c51597d
Nitish Varma <1351705+nitishvarma@users.noreply.github.com>
2021-04-28 21:58:52 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,9 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I am Shalini<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>Mother of two.</h2>
           <SocialList />
         </div>
       </div>
